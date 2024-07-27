22eccd835ff42c536a295ed945caf0acfb1b3889
repo1 <vasudevshreekumar@gmail.com>
2024-07-27 18:40:42 +0530

    some more changes were done

--- Lab2.java
+++ Lab2.java
@@ -1,92 +1,98 @@
 // 1) Automatic type casting
-//public class Lab2
-//{
-//public static void main(String[] args)
-//{
-//final int myInt=15;
-//double myDouble=myInt;//Automatic casting of int to double
-//System.out.print("myInt: "+myInt+"\n");
-//System.out.print("myDouble: "+myDouble+"\n");
-//}
-//}
+// public class Lab2
+// {
+// public static void main(String[] args)
+// {
+// final int myInt=15;
+// double myDouble=myInt;//Automatic casting of int to double
+// System.out.print("myInt: "+myInt+"\n");
+// System.out.print("myDouble: "+myDouble+"\n");
+// }
+// }
 
 // 2) Explicit Type casting
-//public class Lab2{
-//public static void main(String[] args){
-//double mydouble=4.56d;
-//int num=(int)mydouble;
-//System.out.println(mydouble);
-//System.out.println(" "+num);
-//}
-//}
+// public class Lab2{
+// public static void main(String[] args){
+// double mydouble=4.56d;
+// int num=(int)mydouble;
+// System.out.println(mydouble);
+// System.out.println(" "+num);
+// }
+// }
 
 // 3) Final keyword
-//public class Lab2
-//{
-//public static void main(String[] args)
-//{
-//final int myInt=15;
-//System.out.print("myInt: "+myInt+"\n");
-//}
-//}
+// public class Lab2
+// {
+// public static void main(String[] args)
+// {
+// final int myInt=15;
+// System.out.print("myInt: "+myInt+"\n");
+// }
+// }
 
 // 4) + operator
 //to add numbers
-//public class Lab2{
-//public static void main(String[] args){
-//int sum1=23;
-//int sum2=sum1+5;
-//int sum3=sum1+sum2;
-//System.out.println(sum3);
-//}
-//}
+// public class Lab2{
+// public static void main(String[] args){
+// int sum1=23;
+// int sum2=sum1+5;
+// int sum3=sum1+sum2;
+// System.out.println(sum3);
+// }
+// }
 
 // 5) to add strings
-//public class Lab2{
-//public static void main(String[] args){
-//String s1="one";
-//String s2="two";
-//String s3=s1+s2;
-//System.out.println(s3);
+// public class Lab2{
+// public static void main(String[] args){
+// String s1="one";
+// String s2="two";
+// String s3=s1+s2;
+// System.out.println(s3);
+// }
+// }
 
 // 6) Ternary operator
-//public class Lab2{
-//public static void main(String[] args){
-//int x,y;
-//x=30;
-//y=(x==1)?61:90;
-//System.out.println(y+"  ");
-//y=(x==30)?61:90;
-//System.out.println(y);
-//}
-//}
+// public class Lab2{
+// public static void main(String[] args){
+// int x,y;
+// x=30;
+// y=(x==1)?61:90;
+// System.out.println(y+"  ");
+// y=(x==30)?61:90;
+// System.out.println(y);
+// }
+// }
 
 // 7) lenght of a string
-//public class Lab2{
-//public static void main(String[] args){
-//String txt="ABCDEFGHIJK";
-//System.out.println("length of the string is:"+txt.length());
-//}
-//}
+// public class Lab2{
+// public static void main(String[] args){
+// String txt="ABCDEFGHIJK";
+// System.out.println("length of the string is:"+txt.length());
+// }
+// }
 
 // 8) convert string to upper and lower case
-//public class Lab2{
-//public static void main(String[] args){
-//String txt="abcd";
-//System.out.println(txt.toUpperCase());
-//System.out.println(txt.toLowerCase());
-//}
-//}
+// public class Lab2{
+// public static void main(String[] args){
+// String txt="abcd";
+// System.out.println(txt.toUpperCase());
+// System.out.println(txt.toLowerCase());
+// }
+// }
 
 // 9) indexOf method
-//public class Lab2{
-//public static void main(String[] args){
-//String txt="Please locate where 'locate' occurs!";
-//System.out.println(txt.indexOf("locate"));
+// public class Lab2{
+// public static void main(String[] args){
+// String txt="Please locate where 'locate' occurs!";
+// System.out.println(txt.indexOf("locate"));
+// }
+// }
 
 // 10) using char datatype
-//char[] helloArray={'h','e','l','l','o'};
-//String helloString=new String(helloArray);
-//System.out.println(helloString);
-//}
-//}
+// public class Lab2{
+// public static void main(String[] args){
+// char[] helloArray={'h','e','l','l','o'};
+// String helloString=new String(helloArray);
+// System.out.println(helloString);
+// }
+// }
