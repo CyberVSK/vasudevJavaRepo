fca9cd24f985197aafbdfaf2c7863fd58b5ec663
CyberVSK <132339832+CyberVSK@users.noreply.github.com>
2024-07-29 17:10:32 +0530

    Add files via upload

---
+++ week3/String_Builder.java
@@ -0,0 +1,54 @@
+public class String_Buffer{
+    public static void main(String[] args){
+        // 1) append() method
+        // StringBuilder sb=new StringBuilder("Hello");
+        // sb.append("Java"); //now original string is changed
+        // System.out.println(sb);//prints Hello Java
+
+
+        // 2) insert() method in 
+        // StringBuilder sb=new StringBuilder("Hello");
+        // sb.insert(1,"Java");// now original string is changed
+        // System.out.println(sb);//prints HJavaello
+
+
+        // 3) replace() method 
+        // StringBuilder sb=new StringBuilder("Hello");
+        // sb.replace(1,3,"Java");//index 1 ot 3 of Hello will be replaced(excluding 3)
+        // System.out.println(sb);// prints HJavalo
+
+
+        // 4) delete() method
+        // StringBuilder sb=new StringBuilder("Hello");
+        // sb.delete(1,3);//deletes all values from index 1 to 3(excluding 3)
+        // System.out.println(sb);//prints Hlo
+
+
+        // 5) reverse() method 
+        // StringBuilder sb=new StringBuilder("Hello");
+        // sb.reverse();
+        // System.out.println(sb);//prints olleH
+
+
+        // 6) capacity() method
+        // StringBuilder sb=new StringBuilder();
+        // System.out.println(sb.capacity());// default 16
+        // sb.append("Hello");
+        // System.out.println(sb.capacity());//now 16
+        // sb.append("Java is my favourite language");
+        // System.out.println(sb.capacity());//now (16*2)+2=34 i.e (capacity*2)+2
+
+
+        // 7) ensureCapacity() method 
+        // StringBuilder sb=new StringBuilder();
+        // System.out.println(sb.capacity());// default 16
+        // sb.append("Hello");
+        // System.out.println(sb.capacity());//now 16
+        // sb.append("Java is my favourite language");
+        // System.out.println(sb.capacity());//now (16*2)+2=34 i.e (old capacity*2)+2
+        // sb.ensureCapacity(10);//now no change
+        // System.out.println(sb.capacity());//now 34
+        // sb.ensureCapacity(50);//now (34*2)+2=70
+        // System.out.println(sb.capacity());
+    }
+}
