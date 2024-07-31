c213551149d45791a816fd52e736ee85ac2d67d8
CyberVSK <132339832+CyberVSK@users.noreply.github.com>
2024-07-31 15:44:49 +0530

    Add files via upload

---
+++ week3/Ex1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ArraysEx1{
+    public static void main(String[] args){
+        // 1)
+        // int[] array=new int[5];
+        // array[0]=10;
+        // array[1]=20;
+        // array[2]=30;
+        // array[3]=40;
+        // array[4]=50;
+        // for(int i=0;i<5;i++){
+        //     System.out.println("Element at index "+i+": "+array[i]);
+        // }
+
+
+        // 2)
+        
+    }
+}
---
+++ week3/String_Tokenizer.java
@@ -0,0 +1,41 @@
+import java.util.StringTokenizer;
+public class String_Tokenizer{
+    public static void main(String[] args){
+        // 1)
+        // StringTokenizer st = new StringTokenizer("my name is vasudev","v");
+        // while(st.hasMoreTokens()){
+        //     System.out.println(st.nextToken());
+        // }
+
+
+        // 2)
+        // StringTokenizer st=new StringTokenizer("my,name,is,vasudev");
+        // System.out.println("Next token is "+st.nextToken(","));
+
+
+        // 3)
+        // StringTokenizer st = new StringTokenizer("Demonstrating mrthods from String Tokenizer class"," ");
+        // while(st.hasMoreTokens()){
+        //     System.out.println(st.nextToken());
+        // }
+
+
+        // 4)
+        // StringTokenizer st = new StringTokenizer("Demonstrating mrthods from String Tokenizer class"," ");
+        // while(st.hasMoreElements()){
+        //     System.out.println(st.nextToken());
+        // }
+
+
+        // 5)
+        // StringTokenizer st = new StringTokenizer("Demonstrating mrthods from String Tokenizer class"," ");
+        // while(st.hasMoreTokens()){ // to check if the string has any more tokens
+        //     System.out.println(st.nextElement());
+        // }
+
+
+        // 6)
+        // StringTokenizer st = new StringTokenizer("Demonstrating mrthods from String Tokenizer class"," ");
+        // System.out.println("The totalnumber of tokens in the string: "+st.countTokens());
+    }
+}
