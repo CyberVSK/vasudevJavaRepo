318aa5c351268f7e116a3c8d69b243ece6d4021b
CyberVSK <132339832+CyberVSK@users.noreply.github.com>
2024-08-03 12:01:49 +0530

    Update Ex2.java

--- week3/Ex2.java
+++ week3/Ex2.java
@@ -16,7 +16,7 @@ class Student{
     public int roll_no;
     public String name;
     Student(int roll_no,String name){
-        this .roll_no=roll_no;
+        this.roll_no=roll_no;
         this.name=name;
     }
 }
